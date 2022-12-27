e616c68253b253b8694bae9a63212424a0c15018
iArt <iart@aboutdev.io>
2022-12-27 12:35:19 +0700

    Add more tokens

--- src/constants.js
+++ src/constants.js
@@ -1,5 +1,5 @@
 const MESSAGE = ['GM', 'gM', 'Gm', 'GN', 'Gn', 'gN', 'HI', 'Hi']
 const CHANNELS = [process.env.CHANNEL]
-const TOKENS = [process.env.TOKEN]
+const TOKENS = process.env.TOKEN.split(' ')
 
 export { MESSAGE, CHANNELS, TOKENS }
